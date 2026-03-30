9d9a6e119725499d7b9395f435ec7f157f28be0e
david <david.nesterovic@hotmail.com>
2026-03-31 00:48:22 +0200

    admin auth

---
+++ LostAndFoundAPI/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LostAndFoundAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController : ControllerBase
+{
+    [Authorize]
+    [HttpGet("me")]
+    public IActionResult Me()
+    {
+        var email = User.FindFirstValue(ClaimTypes.Email)
+                    ?? User.FindFirstValue("email");
+
+        var isAdmin = User.IsInRole("Admin");
+
+        return Ok(new
+        {
+            email,
+            isAdmin
+        });
+    }
+}
